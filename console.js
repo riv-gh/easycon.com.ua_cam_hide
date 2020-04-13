c7d5998db70e7d1c4380cf655dd97e67ffce7b5f
daenur <daenur@outlook.com>
2020-04-13 15:12:28 +0300

    Create console.js

---
+++ console.js
@@ -0,0 +1,12 @@
+setInterval(function(){
+	$('.joined-to-conference-container>.item, .conference-organizer>.item').not('.patch_my').append('<button class="hide_my">hide/show</button>').addClass('patch_my');
+},1000)
+
+$(document).on('click', '.hide_my', function(){
+	var tmp = $(this).parent().text().trim()
+	$('.user-info').each(function() {
+		if (tmp.indexOf($(this).text()) == 0) {
+			$(this).parent().toggle();
+		}
+	});
+});
